4c290a11fe0fcb398c991181de3c88bdda8d47e8
Enrique Fernandez <kikefdezarevalillo@gmail.com>
2024-11-04 15:58:45 +0100

    first API Call

--- lib/infrastucture/datasources/moviedb_datasource.dart
+++ lib/infrastucture/datasources/moviedb_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastucture/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastucture/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
@@ -19,10 +21,12 @@ class MoviedbDatasource extends MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
 
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> moviesPlaying = [];
-    if(response.statusCode == 200){
 
-    }
-    return moviesPlaying;
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies =  movieDbResponse.results
+    .where((movieDb) => movieDb.posterPath != 'no-poster')
+    .map((movieDb) => MovieMapper.movieDBToEntity(movieDb)).toList();
+    return movies;
   }
 }
---
+++ lib/infrastucture/mappers/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastucture/models/moviedb/movie_from_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieFromMovieDB movieDb) => Movie(
+      adult: movieDb.adult,
+      backdropPath: movieDb.backdropPath != ''
+      ? "https://image.tmdb.org/t/p/w500${movieDb.backdropPath}"
+      : Environment.posterNotFound,
+      genreIds: movieDb.genreIds.map((e) => e.toString()).toList(),
+      id: movieDb.id,
+      originalLanguage: movieDb.originalLanguage,
+      originalTitle: movieDb.originalTitle,
+      overview: movieDb.overview,
+      popularity: movieDb.popularity,
+      posterPath: movieDb.posterPath != ''
+      ? "https://image.tmdb.org/t/p/w500${movieDb.backdropPath}"
+      : Environment.posterNotFound,
+      releaseDate: movieDb.releaseDate,
+      title: movieDb.title,
+      video: movieDb.video,
+      voteAverage: movieDb.voteAverage,
+      voteCount: movieDb.voteCount);
+}
