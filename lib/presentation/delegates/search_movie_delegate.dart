4a704b3e33397a585ed19e6ae00eb71968a11c22
Enrique Fernandez <kikefdezarevalillo@gmail.com>
2024-11-11 11:01:18 +0100

    search delegate only search when stop typing

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/config/helpers/human_format.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../domain/entities/movie.dart';
 
@@ -8,8 +11,31 @@ typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
 
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
   final SearchMoviesCallback searchMovies;
+  StreamController<List<Movie>> debounceMovies = StreamController.broadcast();
+  StreamController<bool> isLoadingMovies = StreamController.broadcast();
+  Timer? _debouceTimer;
 
-  SearchMovieDelegate({required this.searchMovies});
+  SearchMovieDelegate({required this.searchMovies}) : super();
+
+  void _onQueryChanged(String query) {
+    isLoadingMovies.add(true);
+    if (_debouceTimer?.isActive ?? false) _debouceTimer!.cancel();
+
+    _debouceTimer = Timer(const Duration(milliseconds: 500), () async {
+      if (query.isEmpty) {
+        debounceMovies.add([]);
+        return;
+      }
+
+      final movies = await searchMovies(query);
+      debounceMovies.add(movies);
+      isLoadingMovies.add(false);
+    });
+  }
+
+  void clearStreams() {
+    debounceMovies.close();
+  }
 
   @override
   String get searchFieldLabel => 'Buscar Película';
@@ -17,40 +43,78 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
-      FadeIn(
-        animate: query.isNotEmpty,
-        child: IconButton(
-          onPressed: () => query = '',
-          icon: const Icon(Icons.clear),
-        ),
-      ),
+      StreamBuilder(
+          initialData: false,
+          stream: isLoadingMovies.stream,
+          builder: (context, snapshot) {
+            if (snapshot.data ?? false) {
+              return SpinPerfect(
+                duration: const Duration(milliseconds: 800),
+                infinite: true,
+                animate: query.isNotEmpty,
+                child: IconButton(
+                  onPressed: () => query = '',
+                  icon: const Icon(Icons.refresh_rounded),
+                ),
+              );
+            }
+            return FadeIn(
+              animate: query.isNotEmpty,
+              child: IconButton(
+                onPressed: () => query = '',
+                icon: const Icon(Icons.clear),
+              ),
+            );
+          }),
+      
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
-        onPressed: () => close(context, null),
+        onPressed: () {
+          clearStreams();
+          close(context, null);
+        },
         icon: const Icon(Icons.arrow_back_ios));
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    return const Text("buildActions");
+    _onQueryChanged(query);
+    return _ShowMovie(debounceMovies: debounceMovies);
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return FutureBuilder(
-        future: searchMovies(query),
-        builder: (context, snapshot) {
-          final movies = snapshot.data ?? [];
-          return ListView.builder(
-              itemCount: movies.length,
-              itemBuilder: (context, index) {
-                return _MovieItem(movie: movies[index]);
-              });
-        });
+    _onQueryChanged(query);
+    return _ShowMovie(debounceMovies: debounceMovies);
+  }
+}
+
+class _ShowMovie extends StatelessWidget {
+  const _ShowMovie({
+    required this.debounceMovies,
+  });
+
+  final StreamController<List<Movie>> debounceMovies;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: debounceMovies.stream,
+      builder: (context, snapshot) {
+        final movies = snapshot.data ?? [];
+        return ListView.builder(
+            itemCount: movies.length,
+            itemBuilder: (context, index) {
+              return _MovieItem(
+                movie: movies[index],
+              );
+            });
+      },
+    );
   }
 }
 
@@ -62,58 +126,61 @@ class _MovieItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final textStyle = Theme.of(context).textTheme;
     final size = MediaQuery.sizeOf(context);
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: size.width * 0.2,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.network(
-                movie.posterPath,
-                fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () => context.push('/movie-screen/${movie.id}'),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.network(
+                  movie.posterPath,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          const SizedBox(width: 8),
-          SizedBox(
-            width: (size.width - 10) * 0.7,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  movie.title,
-                  style: textStyle.titleMedium,
-                  maxLines: 2,
-                ),
-                Text(
-                  movie.overview,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 3,
-                ),
-                const SizedBox(height: 8),
-                Row(
-                  children: [
-                    Icon(
-                Icons.star_half_outlined,
-                color: Colors.yellow.shade800,
+            const SizedBox(width: 8),
+            SizedBox(
+              width: (size.width - 10) * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    movie.title,
+                    style: textStyle.titleMedium,
+                    maxLines: 2,
+                  ),
+                  Text(
+                    movie.overview,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star_half_outlined,
+                        color: Colors.yellow.shade800,
+                      ),
+                      const SizedBox(
+                        width: 3,
+                      ),
+                      Text(
+                        HumanFormat.numberFormatted(movie.voteAverage, 1),
+                        style: textStyle.bodyMedium
+                            ?.copyWith(color: Colors.yellow.shade800),
+                      ),
+                    ],
+                  )
+                ],
               ),
-              const SizedBox(
-                width: 3,
-              ),
-              Text(
-                HumanFormat.numberFormatted(movie.voteAverage, 1),
-                style: textStyle.bodyMedium
-                    ?.copyWith(color: Colors.yellow.shade800),
-              ),
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -3,6 +3,7 @@ import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+
 class CustomAppBar extends ConsumerWidget {
   const CustomAppBar({super.key});
 
@@ -11,37 +12,39 @@ class CustomAppBar extends ConsumerWidget {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
     return SafeArea(
-        bottom: false,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: SizedBox(
-            width: double.infinity,
-            child: Row(
-              children: [
-                Icon(
-                  Icons.movie_outlined,
-                  color: colors.primary,
-                ),
-                const SizedBox(width: 5),
-                Text(
-                  "Cinemapedia",
-                  style: titleStyle,
-                ),
-                const Spacer(),
-                IconButton(
-                  onPressed: () {
-                    final movieRepository = ref.read(movieRepositoryProvider);
-                    showSearch(
-                      context: context,
-                      delegate: SearchMovieDelegate(
-                          searchMovies: movieRepository.searchMovies),
-                    );
-                  },
-                  icon: Icon(Icons.search),
-                ),
-              ],
-            ),
+      bottom: false,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: SizedBox(
+          width: double.infinity,
+          child: Row(
+            children: [
+              Icon(
+                Icons.movie_outlined,
+                color: colors.primary,
+              ),
+              const SizedBox(width: 5),
+              Text(
+                "Cinemapedia",
+                style: titleStyle,
+              ),
+              const Spacer(),
+              IconButton(
+                onPressed: () {
+                  final movieRepository = ref.read(movieRepositoryProvider);
+                  showSearch(
+                    context: context,
+                    delegate: SearchMovieDelegate(
+                        searchMovies: movieRepository.searchMovies),
+                  );
+
+                },
+                icon: const Icon(Icons.search),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
